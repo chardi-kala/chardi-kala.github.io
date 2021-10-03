7e2528bc1c0415ca82b25b10aa047cb56661665f
The Smiling Sikh <84632186+chardi-kala@users.noreply.github.com>
2021-10-03 23:44:46 +0530

    Update init.js

--- punjabits/init.js
+++ punjabits/init.js
@@ -1,3 +1,13 @@
+export process {
+  env: {
+      AD_ID : secrets.AD_CLIENT_ID
+    }
+}
+export default {
+  env: {
+      AD_ID : secrets.AD_CLIENT_ID
+    }
+}
 window.process = {
     env: {
         AD_ID : secrets.AD_CLIENT_ID
